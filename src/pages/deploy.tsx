e3f3f53a02f1faeaef3b8f2f5ccbf038a157d40c
William Luke <williamluke4@gmail.com>
2023-08-19 11:28:58 +0300

    feat: push to voucher page on deploy

--- src/pages/deploy.tsx
+++ src/pages/deploy.tsx
@@ -3,27 +3,39 @@ import { Card, CardContent } from "~/components/ui/card";
 import { Loading } from "../components/loading";
 import PublishVoucherForm from "../components/voucher/publish-voucher-form";
 import { useDeploy } from "../hooks/useDeploy";
+import { useRouter } from "next/router";
+
 const Home: NextPage = () => {
-  const { deploy, loading, error, receipt, hash, info } = useDeploy();
+  const { deploy, loading, receipt, hash, info, voucher } = useDeploy();
+  const router = useRouter();
+  if(voucher) {
+    void router.push(`/vouchers/${voucher.voucher_address}`);
+  }
+  let content;
+  if (loading) {
+    content = <Loading status={info} />;
+  } else if (receipt) {
+    content = (
+      <div>
+        <p>Transaction Hash: {hash}</p>
+        <div>Contract Address: {receipt.contractAddress}</div>
+      </div>
+    );
+  } else {
+    content = (
+      <PublishVoucherForm
+        onSubmit={(data) => {
+          void deploy(data);
+        }}
+      />
+    );
+  }
 
   return (
-    <div>
-      <div className="container p-4">
-        {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
-        <Card className="pt-4 max-w-lg m-auto">
-          <CardContent>
-            <PublishVoucherForm
-              onSubmit={(data) => {
-                void deploy(data);
-              }}
-            />
-          </CardContent>
-        </Card>
-      </div>
-      {loading && <Loading status={info} />}
-      {hash && <p>Transaction Hash: {hash}</p>}
-      {receipt && <div>Contract Address: {receipt?.contractAddress}</div>}
-      {error && <p>Error: {error.message}</p>}
+    <div className="container p-4">
+      <Card className="pt-4 max-w-lg m-auto">
+        <CardContent>{content}</CardContent>
+      </Card>
     </div>
   );
 };
