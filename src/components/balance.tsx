6cc8bf1781c0da0434a95f2b61acf1d16c5fb959
William Luke <williamluke4@gmail.com>
2023-08-19 11:15:21 +0300

    fix(balance): disable when no address

--- src/components/balance.tsx
+++ src/components/balance.tsx
@@ -1,3 +1,4 @@
+import { isAddress } from "viem";
 import { useBalance } from "wagmi";
 import { toUserUnitsString } from "~/utils/units";
 
@@ -10,6 +11,7 @@ function Balance(props: IBalanceProps) {
   const { data: balance } = useBalance({
     token: props.tokenAddress as `0x${string}`,
     address: props.address as `0x${string}`,
+    enabled: isAddress(props.tokenAddress) && isAddress(props.address),
   });
   return <>{toUserUnitsString(balance?.value, balance?.decimals)}</>;
 }
