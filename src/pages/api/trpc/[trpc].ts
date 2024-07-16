7e6b4a52a3bd45814931ecbeb3281dfe4f4b0b35
William Luke <williamluke4@gmail.com>
2024-07-16 18:37:04 +0300

    fix: increase pool create timeout

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -3,6 +3,11 @@ import { env } from "~/env";
 import { appRouter } from "~/server/api/root";
 import { createTRPCContext } from "~/server/api/trpc";
 
+
+export const config = {
+  maxDuration: 300,
+};
+
 // export API handler
 export default createNextApiHandler({
   router: appRouter,
