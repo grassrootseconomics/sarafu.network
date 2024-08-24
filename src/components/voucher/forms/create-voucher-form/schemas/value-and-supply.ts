c4a5dd26c7bf09609efda5bbed0fa50976f991fa
William Luke <williamluke4@gmail.com>
2024-08-24 12:16:53 +0300

    hotfix: add validation for voucher value Waiting for https://github.com/grassrootseconomics/cic-graph/issues/39

--- src/components/voucher/forms/create-voucher-form/schemas/value-and-supply.ts
+++ src/components/voucher/forms/create-voucher-form/schemas/value-and-supply.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 export const valueAndSupplySchema = z.object({
   uoa: z.string().trim(),
-  value: z.coerce.number(),
+  value: z.coerce.number().int('Value must be an integer. '),
   supply: z.coerce.number(), // Initial Mint
 });
 export type ValueAndSupplyFormValues = z.infer<typeof valueAndSupplySchema>;
