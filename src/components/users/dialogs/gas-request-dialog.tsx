5af5359648b960a07c6828ab3585f29d31a3771f
William Luke <williamluke4@gmail.com>
2024-10-09 09:53:37 +0300

    fix: user queries

--- src/components/users/dialogs/gas-request-dialog.tsx
+++ src/components/users/dialogs/gas-request-dialog.tsx
@@ -1,7 +1,8 @@
 "use client";
 import { useState } from "react";
 import { toast } from "sonner";
-import { Loading } from "~/components/loading";
+import { useAuth } from "~/hooks/useAuth";
+import { trpc } from "~/lib/trpc";
 import { Button } from "../../ui/button";
 import {
   Dialog,
@@ -12,9 +13,8 @@ import {
   DialogTrigger,
 } from "../../ui/dialog";
 import { ProfileForm, type UserProfileFormType } from "../forms/profile-form";
-import { trpc } from "~/lib/trpc";
 const GasRequestDialog = () => {
-  const me = trpc.me.get.useQuery();
+  const auth = useAuth();
   const updateMe = trpc.me.update.useMutation();
   const utils = trpc.useUtils();
   const requestGas = trpc.me.requestGas.useMutation();
@@ -44,17 +44,13 @@ const GasRequestDialog = () => {
             Please update your profile to receive Celo access
           </DialogDescription>
         </DialogHeader>
-        {me.isLoading ? (
-          <Loading />
-        ) : (
-          me.data && (
-            <ProfileForm
-              buttonLabel="Apply and Update Profile"
-              initialValues={me.data}
-              onSubmit={applyAndUpdateProfile}
-              isLoading={updateMe.isPending || requestGas.isPending}
-            />
-          )
+        {auth?.user && (
+          <ProfileForm
+            buttonLabel="Apply and Update Profile"
+            initialValues={auth.user}
+            onSubmit={applyAndUpdateProfile}
+            isLoading={updateMe.isPending || requestGas.isPending}
+          />
         )}
       </DialogContent>
     </Dialog>
