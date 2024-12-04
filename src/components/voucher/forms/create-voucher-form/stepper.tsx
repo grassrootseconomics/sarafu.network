72777bea3dc21a4bb5952325e618310fe4ef929f
William Luke <williamluke4@gmail.com>
2024-12-04 16:01:00 +0300

    chore: create voucher ui improvements

--- src/components/voucher/forms/create-voucher-form/stepper.tsx
+++ src/components/voucher/forms/create-voucher-form/stepper.tsx
@@ -19,13 +19,13 @@ interface StepperProps {
 const Stepper: React.FC<StepperProps> = ({ steps }) => {
   const { activeStep } = useVoucherStepper();
   return (
-    <div className="p-4">
-      <div className="mt-2 flex items-center justify-center">
-        {steps[activeStep]?.icon}
-        <span className="ml-2 text-center">{steps[activeStep]?.label}</span>
-      </div>
-      <div className="mt-4">
-        <div className="relative h-1 rounded">
+    <div>
+      <div className="sticky top-0 flex flex-col bg-white  z-10">
+        <div className="mt-2 flex items-center justify-center">
+          {steps[activeStep]?.icon}
+          <span className="ml-2 text-center">{steps[activeStep]?.label}</span>
+        </div>
+        <div className="relative h-1 rounded w-full py-4">
           {steps.map((step, index) => (
             <div
               key={index}
@@ -43,8 +43,9 @@ const Stepper: React.FC<StepperProps> = ({ steps }) => {
             ></div>
           ))}
         </div>
-
-        <div className="mt-12">{steps[activeStep]?.children}</div>
+      </div>
+      <div className="mt-4">
+        <div className="mt-6">{steps[activeStep]?.children}</div>
       </div>
     </div>
   );
