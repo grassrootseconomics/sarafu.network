38255db12679e04bc8a0132b747a6b303bc6c128
William Luke <williamluke4@gmail.com>
2024-12-09 09:14:40 +0300

    ui: add vpa to receive modal

--- src/components/dialogs/receive-dialog.tsx
+++ src/components/dialogs/receive-dialog.tsx
@@ -9,18 +9,13 @@ import { useState } from "react";
 
 import React from "react";
 import { useAccount } from "wagmi";
+import { useAuth } from "~/hooks/useAuth";
 import useWebShare from "~/hooks/useWebShare";
 import { downloadSVGAsPNG, svgToPNG } from "../../utils/svg-to-png-converter";
 import Address from "../address";
+import { ResponsiveModal } from "../modal";
 import AddressQRCode from "../qr-code/address-qr-code";
 import { Button } from "../ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "../ui/dialog";
 
 interface ReceiveDialogProps {
   button?: React.ReactNode;
@@ -29,6 +24,7 @@ export const ReceiveDialog = (props: ReceiveDialogProps) => {
   const [open, setOpen] = useState(false);
 
   const { address } = useAccount();
+  const auth = useAuth();
   const handleOpenChanged = (open: boolean) => {
     setOpen(open);
   };
@@ -52,55 +48,63 @@ export const ReceiveDialog = (props: ReceiveDialogProps) => {
       });
   };
   return (
-    <Dialog modal open={open} onOpenChange={handleOpenChanged}>
-      <DialogTrigger asChild>
-        {props.button ? (
+    <ResponsiveModal
+      title="Receive"
+      button={
+        props.button ? (
           props.button
         ) : (
           <Button variant={"default"}>
             <PaperPlaneIcon className="m-1" />
           </Button>
-        )}
-      </DialogTrigger>
-      <DialogContent className="max-w-md">
-        <DialogHeader>
-          <DialogTitle>Receive</DialogTitle>
-        </DialogHeader>
-        <div className="flex flex-col space-y-4">
-          <AddressQRCode
-            id="addressQRCodeId"
-            size={256}
-            className="mx-auto"
-            address={address ?? ""}
-          />
-          <Address address={address} className="text-center break-all" />
-          <div className="flex m-2 justify-evenly">
-            {isSupported && (
-              <Button variant="ghost" onClick={handleShare}>
-                <Share1Icon className="mr-2" />
-                Share
-              </Button>
-            )}
-
-            <Button
-              variant="ghost"
-              onClick={() => {
-                downloadSVGAsPNG(
-                  document.getElementById(
-                    "addressQRCodeId"
-                  ) as unknown as SVGSVGElement,
-                  "address.png"
-                ).catch((error) => {
-                  console.error(error);
-                });
-              }}
-            >
-              <DownloadIcon className="mr-2" />
-              Download
-            </Button>
+        )
+      }
+      open={open}
+      onOpenChange={handleOpenChanged}
+    >
+      <div className="flex flex-col space-y-4">
+        {auth?.user?.vpa && (
+          <div className="text-center break-all text-md font-semibold text-gray-800">
+            {auth.user?.vpa}
           </div>
+        )}
+        <Address
+          address={address}
+          className="text-center break-all text-md font-semibold text-gray-700"
+        />
+        <AddressQRCode
+          id="addressQRCodeId"
+          size={256}
+          className="mx-auto"
+          address={address ?? ""}
+        />
+
+        <div className="flex m-2  py-4 justify-evenly">
+          {isSupported && (
+            <Button variant="outline" onClick={handleShare}>
+              <Share1Icon className="mr-2" />
+              Share
+            </Button>
+          )}
+
+          <Button
+            variant="outline"
+            onClick={() => {
+              downloadSVGAsPNG(
+                document.getElementById(
+                  "addressQRCodeId"
+                ) as unknown as SVGSVGElement,
+                "address.png"
+              ).catch((error) => {
+                console.error(error);
+              });
+            }}
+          >
+            <DownloadIcon className="mr-2" />
+            Download
+          </Button>
         </div>
-      </DialogContent>
-    </Dialog>
+      </div>
+    </ResponsiveModal>
   );
 };
