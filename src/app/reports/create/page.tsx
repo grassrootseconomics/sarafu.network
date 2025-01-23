5c094b5fbdb8ad5907842e28c2a92c0228c6c4ae
William Luke <williamluke4@gmail.com>
2025-01-23 15:37:05 +0300

    feat: prevent create when not logged in

--- src/app/reports/create/page.tsx
+++ src/app/reports/create/page.tsx
@@ -1,6 +1,7 @@
 import { type Metadata } from "next";
 import { ContentContainer } from "~/components/layout/content-container";
 import { ReportForm } from "~/components/reports/forms/report-form";
+import { auth } from "~/server/api/auth";
 export const metadata: Metadata = {
   title: "Create Your Own Report",
   description: "Create your own report on the network.",
@@ -9,11 +10,18 @@ export const metadata: Metadata = {
     apple: "/apple-touch-icon.png",
   },
 };
-export default function CreateReportPage() {
+export default async function CreateReportPage() {
+  const session = await auth();
   return (
-    <ContentContainer title="Create Report">
-      <h1 className="text-2xl font-bold my-4">Create Report</h1>
-      <ReportForm />
+    <ContentContainer>
+      {session?.user ? (
+        <>
+          <h1 className="text-2xl font-bold my-4">Create Report</h1>
+          <ReportForm />
+        </>
+      ) : (
+        <div>Please login to create a report</div>
+      )}
     </ContentContainer>
   );
 }
