f2b6836d5b9aa35220a0ecd6d9bc942519ad4eb8
William Luke <williamluke4@gmail.com>
2025-01-29 09:15:07 +0300

    fix: clickable voucher chip

--- src/components/voucher/voucher-chip.tsx
+++ src/components/voucher/voucher-chip.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { cn } from "~/lib/utils";
 import { useVoucherDetails } from "../pools/hooks";
 import { Skeleton } from "../ui/skeleton";
@@ -9,21 +10,30 @@ interface VoucherChipProps {
   voucher_address: `0x${string}`;
   truncate?: boolean;
   className?: string;
+  clickable?: boolean;
 }
 
 export function VoucherChip({
   voucher_address,
   truncate = false,
   className,
+  clickable = false,
 }: VoucherChipProps) {
   const details = useVoucherDetails(voucher_address);
+  const router = useRouter();
   if (details.isLoading) return <Skeleton className="h-6 w-12 rounded-full" />;
   return (
     <div
       className={cn(
         "flex items-center gap-2 pr-4 bg-muted rounded-full",
+        clickable && "cursor-pointer",
         className
       )}
+      onClick={() => {
+        if (clickable) {
+          router.push(`/vouchers/${voucher_address}`);
+        }
+      }}
     >
       <VoucherIcon voucher_address={voucher_address} className="size-8" />
       <span className="flex flex-col w-full items-start text-xs">
