da0a6f2dead3e9483a388ffd124f9c5889b634b0
William Luke <williamluke4@gmail.com>
2025-03-19 15:48:11 +0300

    feat: speed up pool creation

--- src/contracts/writer.ts
+++ src/contracts/writer.ts
@@ -1,14 +1,18 @@
 import { createWalletClient, http } from "viem";
-import { privateKeyToAccount } from "viem/accounts";
+import { nonceManager, privateKeyToAccount } from "viem/accounts";
 import { celo } from "viem/chains";
 import { env } from "~/env";
 
 export function getWriterAccount() {
   const { WRITER_PRIVATE_KEY } = env;
-  return privateKeyToAccount(WRITER_PRIVATE_KEY as `0x${string}`);
+  return privateKeyToAccount(WRITER_PRIVATE_KEY as `0x${string}`, {
+    nonceManager,
+  });
 }
+
 export function getWriterWalletClient() {
   const account = getWriterAccount();
+
   return createWalletClient({
     account: account,
     chain: celo,
