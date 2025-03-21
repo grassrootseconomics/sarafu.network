2b505c9bfd1454d3f5121593d5b0562c955badf2
William Luke <williamluke4@gmail.com>
2025-03-21 07:58:08 +0300

    feat: update UserGasStatus component and integrate it into the sidebar

--- src/app/wallet/page.tsx
+++ src/app/wallet/page.tsx
@@ -1,12 +1,10 @@
 import { ContentContainer } from "~/components/layout/content-container";
-import UserGasStatus from "~/components/users/user-gas-status";
 import WalletHome from "~/components/wallet/WalletHome";
 
 const WalletPage = () => {
   return (
     <ContentContainer title="Wallet">
       <div className="max-w-lg w-full flex flex-col flex-grow mx-auto px-1 sm:px-2">
-        <UserGasStatus />
         <WalletHome />
       </div>
     </ContentContainer>
