dcb7f526cf66644648d92725cae38d9c899ff7c4
Mohammed Sohail <sohailsameja@gmail.com>
2025-06-10 10:39:48 +0300

    fix: revert to fallback strat

--- src/config/viem.config.server.ts
+++ src/config/viem.config.server.ts
@@ -1,4 +1,4 @@
-import { createPublicClient, http } from "viem";
+import { createPublicClient, fallback, http } from "viem";
 import { celo } from "wagmi/chains";
 
 // Get projectId from https://cloud.reown.com
@@ -6,7 +6,11 @@ export const projectId = "26d03a81230d2bcd268e0434bec65f3a";
 
 export const appName = "Sarafu.Network";
 
-export const celoTransport = http("https://celo.sarafu.africa")
+export const celoTransport = fallback([
+  http("https://celo.sarafu.africa"),
+  http(),
+]);
+
 export const publicClient = createPublicClient({
   chain: celo,
   transport: celoTransport,
