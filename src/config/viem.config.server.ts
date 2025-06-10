9d1c0eab965e52f391391e604861a5ada01e0946
Mohammed Sohail <sohailsameja@gmail.com>
2025-06-10 10:30:24 +0300

    feat: use globally loadbalanced Celo RPC

--- src/config/viem.config.server.ts
+++ src/config/viem.config.server.ts
@@ -1,4 +1,4 @@
-import { createPublicClient, fallback, http } from "viem";
+import { createPublicClient, http } from "viem";
 import { celo } from "wagmi/chains";
 
 // Get projectId from https://cloud.reown.com
@@ -6,13 +6,7 @@ export const projectId = "26d03a81230d2bcd268e0434bec65f3a";
 
 export const appName = "Sarafu.Network";
 
-export const celoTransport = fallback([
-  http("https://r1-celo.grassecon.org/"),
-  http("https://r2-celo.grassecon.org/"),
-  http("https://r3-celo.grassecon.org/"),
-  http("https://r4-celo.grassecon.org/"),
-  http(),
-]);
+export const celoTransport = http("https://celo.sarafu.africa")
 export const publicClient = createPublicClient({
   chain: celo,
   transport: celoTransport,
