c493327a5867c05409185ffc0af613ff33ab91ea
William Luke <williamluke4@gmail.com>
2025-11-04 13:34:29 +0300

    refactor: optimize voucher icon loading by integrating intersection observer for lazy loading based on visibility

--- src/components/voucher/voucher-icon.tsx
+++ src/components/voucher/voucher-icon.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { useInView } from "react-intersection-observer";
 import { trpc } from "~/lib/trpc";
 import { cn } from "~/lib/utils";
 export function VoucherIcon({
@@ -8,17 +9,23 @@ export function VoucherIcon({
   voucher_address: `0x${string}`;
   className?: string;
 }) {
+  const { ref, inView } = useInView({
+    triggerOnce: true, // only run once when visible
+    threshold: 0.1, // 10% visible
+  });
+
   const voucher = trpc.voucher.byAddress.useQuery(
     {
       voucherAddress: voucher_address,
     },
     {
-      enabled: !!voucher_address,
+      enabled: !!voucher_address && inView,
       staleTime: Infinity,
     }
   );
   return (
     <Image
+      ref={ref}
       src={voucher.data?.icon_url ?? "/apple-touch-icon.png"}
       alt={`${voucher.data?.voucher_name}`}
       width={24}
