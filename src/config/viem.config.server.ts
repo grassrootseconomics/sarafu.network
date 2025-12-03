962484a4e7589c8073fd7a8b903de435b39b7683
William Luke <williamluke4@gmail.com>
2025-12-03 11:13:15 +0300

    hotfix: disable r4

--- src/config/viem.config.server.ts
+++ src/config/viem.config.server.ts
@@ -1,27 +1,25 @@
 import { createPublicClient, fallback, http } from "viem";
 import { celo } from "wagmi/chains";
 
-
 export const defaultReceiptOptions = {
   retryCount: 10,
   confirmations: 5,
   retryDelay: 1000,
   pollingInterval: 1000,
-}
+};
 // Get projectId from https://cloud.reown.com
 export const projectId = "26d03a81230d2bcd268e0434bec65f3a";
 
 export const appName = "Sarafu.Network";
 
 export const celoTransport = fallback([
-  http("https://r4-celo.grassecon.org"),
+  // http("https://r4-celo.grassecon.org"),
   http(),
 ]);
 
 export const publicClient = createPublicClient({
   chain: celo,
   transport: celoTransport,
-
 });
 
 // This CeloChain type will now also be based on our specific celo object
