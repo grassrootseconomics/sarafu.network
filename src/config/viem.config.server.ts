463490c3fbdd00d82eb2db87932b3fcde4434bcd
William Luke <williamluke4@gmail.com>
2026-01-07 08:37:48 +0000

    fix: add r4 as Celo transport

--- src/config/viem.config.server.ts
+++ src/config/viem.config.server.ts
@@ -13,6 +13,7 @@ export const projectId = "26d03a81230d2bcd268e0434bec65f3a";
 export const appName = "Sarafu.Network";
 
 export const celoTransport = fallback([
+  http("https://r4-celo.grassecon.org"),
   http("https://r3-celo.grassecon.org"),
   http(),
 ]);
