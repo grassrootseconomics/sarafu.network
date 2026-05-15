56d7cb288161936b3cd5c9db132d443ebd097a22
Will Ruddick <willruddick@gmail.com>
2026-05-16 01:06:27 +0300

    fix: update About link to use relative URL in navigation

--- src/config/site.tsx
+++ src/config/site.tsx
@@ -144,7 +144,7 @@ const siteConfig: {
         {
           icon: <BookOpen size={18} />,
           title: "About",
-          href: "https://docs.grassecon.org/",
+          href: "/about",
           description: "Learn about Grassroots Economics",
         },
         {
